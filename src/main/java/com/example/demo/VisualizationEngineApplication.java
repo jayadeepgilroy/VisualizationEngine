648d90eaf1a4d80e555e77749cbb6fa31d6d936f
Jayadeep Gilroy <jayadeep.gilroy@altisource.com>
2018-02-19 14:49:56 -0500

    adding jfreechart service

--- src/main/java/com/example/demo/VisualizationEngineApplication.java
+++ src/main/java/com/example/demo/VisualizationEngineApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.InputStreamResource;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -32,7 +37,7 @@ public class VisualizationEngineApplication {
 }
 
 @Service
-class HTMLToPDFService {
+class ChartService {
 
 	public ByteArrayInputStream convertHTMLToPDF() throws DocumentException, IOException {
 
@@ -55,14 +60,49 @@ class HTMLToPDFService {
 
 	}
 
+	public ByteArrayInputStream createChartPDF() throws DocumentException, IOException {
+
+		Document document = new Document();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		PdfWriter writer = PdfWriter.getInstance(document, out);
+		document.open();
+		int width = 300;
+		int height = 300;
+		JFreeChart chart = getChart();
+		BufferedImage bufferedImage = chart.createBufferedImage(width, height);
+		Image image = Image.getInstance(writer, bufferedImage, 1.0f);
+
+		
+		document.add(image);
+
+		document.close();
+		return new ByteArrayInputStream(out.toByteArray());
+
+	}
+
+	public JFreeChart getChart() {
+
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		dataset.setValue("One", new Double(43.2));
+		dataset.setValue("Two", new Double(10.0));
+		dataset.setValue("Three", new Double(27.5));
+		dataset.setValue("Four", new Double(17.5));
+		dataset.setValue("Five", new Double(11.0));
+		dataset.setValue("Six", new Double(19.4));
+
+		// use the ChartFactory to create a pie chart
+		JFreeChart chart = ChartFactory.createPieChart("Dummy Data", dataset, true, true, false);
+		return chart;
+	}
+
 }
 
 @Controller
 class VisualizationEngineController {
 
-	public HTMLToPDFService htmlToPDFService = null;
+	public ChartService htmlToPDFService = null;
 
-	public VisualizationEngineController(HTMLToPDFService htmlToPDFService) {
+	public VisualizationEngineController(ChartService htmlToPDFService) {
 		this.htmlToPDFService = htmlToPDFService;
 	}
 
@@ -71,7 +111,18 @@ class VisualizationEngineController {
 
 		ByteArrayInputStream bis = htmlToPDFService.convertHTMLToPDF();
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
+		headers.add("Content-Disposition", "inline; filename=test.pdf");
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).headers(headers)
+				.body(new InputStreamResource(bis));
+
+	}
+
+	@GetMapping(value = "/report1", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<InputStreamResource> createChartReport() throws DocumentException, IOException {
+
+		ByteArrayInputStream bis = htmlToPDFService.createChartPDF();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=test.pdf");
 		return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).headers(headers)
 				.body(new InputStreamResource(bis));
 
